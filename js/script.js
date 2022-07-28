cd4fd6cfb3863ac2c0339018ece3fb91bcf73749
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-07-28 12:28:08 -0300

    mudei a nav agora a animação ocorre ao clique no link da nav

--- js/script.js
+++ js/script.js
@@ -1,30 +1,35 @@
 // Animação da Logo
 function logoAdd() {
-const path = document.querySelectorAll('path');
-const logo = document.querySelector('.logo');
-
-window.onload = () => {
-  setTimeout(() => {
-    path.forEach((item) => {
-      item.setAttribute('fill', '#BDFF02');
-      logo.classList.add('sombra')
-    });
-  }, 1350);
-};
+  const path = document.querySelectorAll('path');
+  const logo = document.querySelector('.logo');
+  
+  window.onload = () => {
+    setTimeout(() => {
+      path.forEach((item) => {
+        item.setAttribute('fill', '#BDFF02');
+        logo.classList.add('sombra')
+      });
+    }, 1350);
+  };
 };
 logoAdd();
 
 // Active
-const nav = document.querySelectorAll('#nav li a');
-const tabSlide = document.querySelector('.tabSlide')
+function navSlide() {
+  const nav = document.querySelectorAll('#nav li a');
+  const tabSlide = document.querySelector('.tabSlide');
 
+  let navTeste = ['home', 'servicos', 'comunidade', 'dev'];
 
-nav.forEach((e, index) => {
-  e.addEventListener('click', () => {
-    nav.forEach((event) => {
-      event.classList.remove('active');
+  nav.forEach((e, index) => {
+    e.addEventListener('click', () => {
+      tabSlide.classList.remove('home', 'servicos', 'comunidade', 'dev');
+      tabSlide.classList.add(navTeste[index]);
+      nav.forEach((event) => {
+        event.classList.remove('active');
+      });
+      e.classList.add('active');
     });
-    e.classList.add('active');
-    console.log(index);
   });
-});
+};
+navSlide();
